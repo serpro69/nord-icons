3ec550427c90f4ddcb1589bd5eea7cf639a42c60
Serhii Prodan <serpro@disroot.org>
2022-06-26 19:59:59 +0200

    Add unmarshaling func for svg files

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,12 +12,43 @@ import (
 func main() {
 	files := readFiles("candy-icons/apps/scalable")
 
-	for _, v := range files {
-		fmt.Println(v)
-	}
+	fmt.Println(unmarshalSvg(files[0]))
 }
 
-func readFiles(in string) map[os.DirEntry]string {
+func unmarshalSvg(filePath string) svg {
+	var icon svg
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = xml.Unmarshal(data, &icon)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return icon
+}
+
+type rawXML struct {
+	Inner []byte `xml:",innerxml"`
+}
+
+type svg struct {
+	XMLName        xml.Name         `xml:"svg"`
+	LinearGradient []linearGradient `xml:"linearGradient"`
+	Path           []rawXML         `xml:"path"`
+}
+
+type linearGradient struct {
+	Stop []stop `xml:"stop"`
+}
+
+type stop struct {
+	Offset      string `xml:"offset,attr"`
+	StopOpacity string `xml:"stop-opacity,attr"`
+	Style       string `xml:"style,attr"`
+}
+
+func readFiles(in string) []string {
 	f, err := os.Open(in)
 	if err != nil {
 		fmt.Println(err)
@@ -34,11 +67,12 @@ func readFiles(in string) map[os.DirEntry]string {
 		panic("Error getting absolute path")
 	}
 
-	out := make(map[os.DirEntry]string)
+	//out := make(map[os.DirEntry]string)
+	var out []string
 
 	for _, file := range files {
 		if extension(file.Name()) == "svg" && file.Type().IsRegular() {
-			out[file] = filepath.Join(path, file.Name())
+			out = append(out, filepath.Join(path, file.Name()))
 		}
 	}
 
